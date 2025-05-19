1cb820875998a54aaa78db62c1153f6bd57679e1
clairebadger <claireebadger@gmail.com>
2025-05-18 19:27:16 -0600

    changed adapter and organized css

--- drawgen/src/routes/+page.svelte
+++ drawgen/src/routes/+page.svelte
@@ -23,34 +23,6 @@
 </div>
 
 <style lang="postcss">
-    .grid-container {
-        height: 90vh;
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        grid-auto-rows: minmax(100px, auto);
-        gap: 5%;
-        padding: 7% 1.5em 1.5em;
-        font: .8em "Chillax", sans-serif;
-    }
-    .span-col {
-        grid-column: auto / span 2;
-    }
-    img {
-        grid-row: auto / span 2;
-        border-radius: 1%;
-    }
-    .info-section {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        font: 4em sans-serif;
-        height: 40vh;
-    }
-    .details {
-        font: .5em "Chillax", sans-serif;
-    }
-    .buttons {
-        display: flex;
-        justify-content: space-evenly;
-    }
+    @import '../css/Shared.css';
+    @import '../css/Page.css';
 </style>
